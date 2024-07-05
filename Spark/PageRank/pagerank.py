e6698ff0b0e4f28a929bdc240ccce19999269aa7
ssewit <105317921+ssewit@users.noreply.github.com>
2024-07-04 19:48:15 -0700

    Add files via upload

---
+++ Spark/PageRank/pagerank.py
@@ -0,0 +1,54 @@
+import re
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+def computeContribs(urls, rank):
+    """Calculates URL contributions to the rank of other URLs."""
+    num_urls = len(urls)
+    for url in urls:
+        yield (url, rank / num_urls)
+
+
+def parseNeighbors(urls):
+    """Parses a urls pair string into urls pair."""
+    parts = re.split(r'\s+', urls)
+    return parts[0], parts[1]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: pagerank <file> <iterations>", file=sys.stderr)
+        sys.exit(-1)
+
+    # Initialize the spark context.
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPageRank")\
+        .getOrCreate()
+
+    # Loads in input file
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+
+    # Loads all URLs from input file and initialize their neighbors.
+    links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cache()
+
+    # Loads all URLs and initialize ranks of them to one.
+    ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
+
+    # Calculates and updates URL ranks continuously using PageRank algorithm.
+    for iteration in range(int(sys.argv[2])):
+        # Calculates URL contributions to the rank of other URLs.
+        contribs = links.join(ranks).flatMap(
+            lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+
+        # Re-calculates URL ranks based on neighbor contributions.
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
+
+    # Collects all URL ranks and dump them to console.
+    for (link, rank) in ranks.collect():
+        print("%s has rank: %s." % (link, rank))
+
+    spark.stop()
